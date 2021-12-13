e84b3a4f1fa314b6b45bc3f6dfc1803006fb6e28
adhithin <adhithi.nmurthy07@gmail.com>
2021-12-13 08:41:29 -0800

    button toggle to masked

--- src/main/java/com/nighthawk/csa/PersonSqlMvcController.java
+++ src/main/java/com/nighthawk/csa/PersonSqlMvcController.java
@@ -11,6 +11,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 import javax.validation.Valid;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -37,7 +38,7 @@ public class PersonSqlMvcController implements WebMvcConfigurer {
         @param - Person Class
     */
     @GetMapping("/createPerson")
-    public String createPerson(Person person) {
+    public String createPerson(Person Person, Model model) {
         return "createPerson";
     }
 
