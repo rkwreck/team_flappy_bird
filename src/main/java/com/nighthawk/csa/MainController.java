da5357d23a155cee0650749f9c3abfae8095c862
adhithin <adhithi.nmurthy07@gmail.com>
2021-12-14 09:20:36 -0800

    running draw

--- src/main/java/com/nighthawk/csa/MainController.java
+++ src/main/java/com/nighthawk/csa/MainController.java
@@ -58,6 +58,11 @@ public class MainController {
         return "game3";
     }
 
+    @GetMapping("/draw")
+    public String draw(Model model) {
+        return "draw";
+    }
+
     @GetMapping("/greetAdi")
     public String apiAdi(@RequestParam(name="sym", required=false, defaultValue="stomach") String name, Model model) throws IOException, InterruptedException {
         String rapidapiurl = "https://healthwise.p.rapidapi.com/body/diseases/" + name;
