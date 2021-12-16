d028defe6f6012253a8d8b3a7b14d3eaded076fb
adhithin <adhithi.nmurthy07@gmail.com>
2021-12-15 21:19:10 -0800

    draw file

---
+++ src/main/java/com/nighthawk/csa/data/SQL/draw.java
@@ -0,0 +1,16 @@
+package com.nighthawk.csa.data.SQL;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class draw extends JPanel{
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        // painting code goes here.
+    }
+
+    
+}
