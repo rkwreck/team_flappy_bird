98d54b2dc88e4cb1fbcc5568ba5ee7de873e13a9
Prisha Maiti <prisha.maiti@gmail.com>
2022-01-03 09:16:06 -0800

    Updated Trivia.java

--- src/main/java/com/nighthawk/csa/Trivia.java
+++ src/main/java/com/nighthawk/csa/Trivia.java
@@ -7,17 +7,7 @@ public class Trivia {
     public Trivia(String q1, String d) {
     }
 
-    public static void main (String[] args) {
-        String q1 = "Under what condition are you allowed to use the 4 main kinematics equations?\n"
-                + "(a)When time is constant\n(b)When position is constant\n(c)When velocity is constant]\n(d)When acceleration is constant\n";
-        String q2 = "If a projectile is launched at an angle and returns back to the same height when it lands, what type of projectile motion is it?\n"
-                + "(a)Type 1\n(b)Type 2\n(c)Type 3\n";
-        Trivia [] questions = {
-                new Trivia(q1, "d"),
-                new Trivia(q2, "b"),
-        };
-        takeTest(questions);
-    }
+
 
     public static void takeTest(Trivia[] questions) {
         int score = 0;
@@ -30,6 +20,18 @@ public class Trivia {
             }
         }
         System.out.println("You got " + score + "/" + questions.length);
+    }
+
+    public static void main (String[] args) {
+        String q1 = "Under what condition are you allowed to use the 4 main kinematics equations?\n"
+                + "(a)When time is constant\n(b)When position is constant\n(c)When velocity is constant]\n(d)When acceleration is constant\n";
+        String q2 = "If a projectile is launched at an angle and returns back to the same height when it lands, what type of projectile motion is it?\n"
+                + "(a)Type 1\n(b)Type 2\n(c)Type 3\n";
+        Trivia [] questions = {
+                new Trivia(q1, "d"),
+                new Trivia(q2, "b"),
+        };
+        takeTest(questions);
     }
 
 }
