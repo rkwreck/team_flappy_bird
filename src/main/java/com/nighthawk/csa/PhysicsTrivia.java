cb0f0c525223569d521c9a47cd426a3c09d76930
Prisha Maiti <prisha.maiti@gmail.com>
2022-01-04 09:51:30 -0800

    Added Question 3 to PhysicsTrivia.java

--- src/main/java/com/nighthawk/csa/PhysicsTrivia.java
+++ src/main/java/com/nighthawk/csa/PhysicsTrivia.java
@@ -4,7 +4,7 @@ public class PhysicsTrivia {
     private boolean prompt;
     private String answer;
 
-    public PhysicsTrivia(String q1, String d) {
+    public PhysicsTrivia(String q1, String correctAnswer) {
     }
 
 
@@ -27,9 +27,12 @@ public class PhysicsTrivia {
                 + "(a)When time is constant\n(b)When position is constant\n(c)When velocity is constant]\n(d)When acceleration is constant\n";
         String q2 = "If a projectile is launched at an angle and returns back to the same height when it lands, what type of projectile motion is it?\n"
                 + "(a)Type 1\n(b)Type 2\n(c)Type 3\n";
+        String q3 = "A 2.0 kg block is initially at rest on a frictionless surface. A force F changes its velocity to 5.0 m/s in 4 seconds. What is the block's acceleration?\n"
+                + "(a)1.25 m/s\n(b)1.25 m/s^2\n(c)1.5 m/s\n(d)1.5 m/s^2";
         PhysicsTrivia [] questions = {
                 new PhysicsTrivia(q1, "d"),
                 new PhysicsTrivia(q2, "b"),
+                new PhysicsTrivia(q3, "b"),
         };
         takeTest(questions);
     }
