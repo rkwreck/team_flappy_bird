bc2f184a89e72bc9f0f5694069f95b15003bb81d
adhithin <adhithi.nmurthy07@gmail.com>
2022-01-19 18:36:12 -0800

    data base files

---
+++ src/main/java/com/nighthawk/csa/frq6.java
@@ -0,0 +1,23 @@
+package com.nighthawk.csa;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+import java.util.*;
+import java.util.function.Supplier;
+
+public class frq6 {
+
+    List str = new ArrayList();
+
+    public String finder() {
+        for (int i = 0; i < str.size(); i++) {
+            System.out.print("yes" + i);
+
+
+        }
+
+
+        return null;
+    }
+}
