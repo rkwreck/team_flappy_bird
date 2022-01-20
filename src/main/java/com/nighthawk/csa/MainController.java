e69d1dd6ee5d94f84a68bd7a2866ab9997b194f2
adhithin <adhithi.nmurthy07@gmail.com>
2022-01-20 15:12:21 -0800

    redirected controllers to proper html pages

--- src/main/java/com/nighthawk/csa/MainController.java
+++ src/main/java/com/nighthawk/csa/MainController.java
@@ -33,34 +33,34 @@ public class MainController {
 
     @GetMapping("/aboutus")
     public String aboutus(Model model) {
-        return "aboutus";
+        return "aboutus/aboutus";
     }
 
     @GetMapping("/games")
     public String games(Model model) {
-        return "games";
+        return "games/games";
     }
 
     @GetMapping("/game1")
     public String game1(Model model) {
-        return "game1";
+        return "games/game1";
     }
 
     @GetMapping("/game2")
     public String game2(Model model) {
-        return "game2";
+        return "games/game2";
     }
 
     @GetMapping("/game3")
     public String game3(Model model) {
-        return "game3";
+        return "games/game3";
     }
 
     @GetMapping("/game4")
-    public String game4(Model model) {return "game4";}
+    public String game4(Model model) {return "games/game4";}
 
     @GetMapping("/game5")
-    public String game5(Model model) {return "game5";}
+    public String game5(Model model) {return "games/game5";}
 
     @GetMapping("/draw")
     public String draw(Model model) {
@@ -97,7 +97,7 @@ public class MainController {
         model.addAttribute("map", amap);
         model.addAttribute("stuff", amap.get("data"));
 
-        return "greetAdi";
+        return "aboutus/greetAdi";
     }
 
     @GetMapping("/greetChenxin")
@@ -119,7 +119,7 @@ public class MainController {
         model.addAttribute("publisher", map.get("publisher"));
         model.addAttribute("date", map.get("release_date"));
 
-        return "greetChenxin";
+        return "aboutus/greetChenxin";
     }
 
     @GetMapping("/greetPrisha")
@@ -137,7 +137,7 @@ public class MainController {
         model.addAttribute("map", pmap);
         model.addAttribute("stuff", pmap.get("data"));
 
-        return "greetPrisha";
+        return "aboutus/greetPrisha";
     }
 
     @GetMapping("/greetRini")
@@ -155,7 +155,7 @@ public class MainController {
         model.addAttribute("map", rmap);
         model.addAttribute("stuff", rmap.get("response"));
 
-        return "greetRini";
+        return "aboutus/greetRini";
     }
 
     @GetMapping("/physicsTrivia")
@@ -205,22 +205,22 @@ public class MainController {
 
     @GetMapping("/frq_rini")
     public String frqRini(Model model) {
-        return "frq_rini";
+        return "frqs/frq_rini";
     }
 
     @GetMapping("/frq_adi")
     public String frqAdi(Model model) {
-        return "frq_adi";
+        return "frqs/frq_adi";
     }
 
     @GetMapping("/frq_chenxin")
     public String frqChenxin(Model model) {
-        return "frq_chenxin";
+        return "frqs/frq_chenxin";
     }
 
     @GetMapping("/frq_prisha")
     public String frqPrisha(Model model) {
-        return "frq_prisha";
+        return "frqs/frq_prisha";
     }
 
 }
