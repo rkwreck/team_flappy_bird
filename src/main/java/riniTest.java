e89af4608f2b9707e379aca372dcd6e917b2fe86
Rini K <89210546+rkwreck@users.noreply.github.com>
2022-02-15 10:01:59 -0800

    made some changes to riniTest

--- src/main/java/riniTest.java
+++ src/main/java/riniTest.java
@@ -24,10 +24,6 @@ public class riniTest {
         return squirrels;
     }
 
-    ArrayList Index = new ArrayList<>(); //initialize Index array
-    //Index.add
-
-
     // It contains a getter for Index, or the order it was initialized
     public int getIndex() {
         //return
@@ -37,16 +33,46 @@ public class riniTest {
     // Write a tester method
     public static void main(String[] args) {
 
+        //create ArrayList Index to keep track of when squirrelswere aded
+        ArrayList<Integer> Index = new ArrayList<Integer>();
+
         // Create an ArrayList of Type Number, my ArrayList is called squirrels
         ArrayList squirrels;
 
         // Initialize 10 squirrels of class type Number
         squirrels = new ArrayList<riniTest>(10);
+        Index.add(1);
+
+        //Initialize Sample Output
+        ArrayList squirrelOne = new ArrayList<riniTest>(3);
+        Index.add(9);
+
+        ArrayList squirrelTwo = new ArrayList<riniTest>(8);
+        Index.add(0);
+
+        ArrayList squirrelThree = new ArrayList<riniTest>(13);
+        Index.add(7);
+
+        ArrayList squirrelFive = new ArrayList<riniTest>(19);
+        Index.add(6);
+
+        ArrayList squirrelSix = new ArrayList<riniTest>(24);
+        Index.add(5);
+
+        ArrayList squirrelSeven = new ArrayList<riniTest>(25);
+        Index.add(2);
+
+        ArrayList squirrelEight = new ArrayList<riniTest>(28);
+        Index.add(8);
+
+        ArrayList squirrelNine = new ArrayList<riniTest>(32);
+        Index.add(4);
+
+        ArrayList squirrelTen = new ArrayList<riniTest>(34);
+        Index.add(3);
 
         // Insert Number instance into ArrayList Squirrel in least to greatest order by random number, mine required nested loops
         ArrayList Squirrel;
-
-
 
         // Print a formatted message with number of Squirrels and Index by which they were created, use enhanced for loop
         System.out.println("This is the number of Squirrels and Index by which they were created.");
